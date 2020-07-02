e97823d16762b2db88b4d7805591167844305afc
wuxingheng <1228678518@qq.com>
2020-07-02 18:13:43 +0800

    :fire:

--- server/update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/expect -f
-set timeout -1
-set password PAsscloud
-# spawn scp -r ./dist/. passcloud@192.168.110.75:/nginx/html/fed/hoslink-daas/.
-spawn scp -r ./public/. passcloud@192.168.110.75:/nginx/html/fed/fed-node-upload/.
-expect "*assword:*"
-send "$password\r"
-interact
